2d2ecc1f5641858e0f7952bed1215bc51351f929
pixel-42-dev <vadim.tkachev.42@gmail.com>
2022-12-28 13:45:09 +0300

    lab14

---
+++ lab_14/script.js
@@ -0,0 +1,24 @@
+const amount = document.getElementById('amount');
+const guest = document.getElementById('guest');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+function calculate() {
+    const tip = ((amount.value * quality.value) / (guest.value)).toFixed(2);
+    amount.value = '';
+    guest.value = '';
+    quality.value = '';
+    if (tip === 'NaN') {
+        tipAmount.innerHTML = "Ошибка, не введены данные";
+        showTipAmount();
+    } else {
+        tipAmount.innerHTML = tip + "Р с каждого";
+        showTipAmount();
+    }
+}
+
+function showTipAmount() {
+    var x = tipAmount;
+    x.className = 'show';
+    setTimeout(function(){x.className = x.className.replace('show', '');}, 3000);
+}
